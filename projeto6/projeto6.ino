b4301d1047a1a289a5b98844305d5bade0f81d41
thiago.p.magalhaes <thiago.p.magalhaes@gmail.com>
2012-12-28 23:36:21 +0000

    Novo projeto.

---
+++ projeto6/projeto6.ino
@@ -0,0 +1,23 @@
+//projeto - 6 - Efeito sequencial Leds
+//principal aprendizado uso de vetores
+
+/*
+ * Principio a cada determinado periodo de tempo os leds irão apagando e acendendo
+ * esse tempo é obtido pela função milissegundos
+ *
+ */
+
+int ledPin = 10;
+//The setup function is called once at startup of the sketch
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+    digitalWrite(ledPin,HIGH);
+//Add your repeated code here
+}
